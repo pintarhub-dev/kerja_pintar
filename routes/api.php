41001d75a2fe556dd71ed8b0ab3229ddc918514d
pintarhub-dev <pintarhub@gmail.com>
2026-01-28 14:42:56 +0700

    Grouping API

--- routes/api.php
+++ routes/api.php
@@ -30,18 +30,24 @@ Route::prefix('v1')->group(function () {
     // --- PROTECTED ROUTES (Butuh Token) ---
     Route::middleware('auth:sanctum')->group(function () {
         Route::post('auth/logout', [AuthController::class, 'logout']);
+
         Route::prefix('attendance')->controller(AttendanceController::class)->group(function () {
             Route::post('clock-in', 'clockIn');
             Route::post('clock-out', 'clockOut');
         });
 
         Route::get('/leaves/balances', [LeaveBalanceController::class, 'index']);
-        Route::get('/leaves/requests', [LeaveRequestController::class, 'index']);
-        Route::post('/leaves/requests', [LeaveRequestController::class, 'store']);
-        Route::delete('/leaves/requests/{id}', [LeaveRequestController::class, 'destroy']);
 
-        Route::get('/overtimes/requests', [OvertimeRequestController::class, 'index']);
-        Route::post('/overtimes/requests', [OvertimeRequestController::class, 'store']);
-        Route::delete('/overtimes/requests/{id}', [OvertimeRequestController::class, 'destroy']);
+        Route::prefix('leaves')->controller(LeaveRequestController::class)->group(function () {
+            Route::get('requests', 'index');
+            Route::post('requests', 'store');
+            Route::delete('requests/{id}', 'destroy');
+        });
+
+        Route::prefix('overtimes')->controller(OvertimeRequestController::class)->group(function () {
+            Route::get('requests', 'index');
+            Route::post('requests', 'store');
+            Route::delete('requests/{id}', 'destroy');
+        });
     });
 });
