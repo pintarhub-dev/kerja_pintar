7e784b7c80c0bbd538740a7e0513acbd2709383a
pintarhub-dev <pintarhub@gmail.com>
2026-01-28 14:32:42 +0700

    Pendaftaran schedule code automation

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+
+        Schedule::command('attendance:auto-clockout')->dailyAt('23:55');
+        Schedule::command('attendance:auto-alpha')->dailyAt('23:59');
     }
 
     /**
@@ -20,7 +23,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
